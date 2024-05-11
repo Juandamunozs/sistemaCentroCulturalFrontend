149584b9607717736d0f0292d1157af52423d17f
juandamunozs <juandavidmunozsotelo7@gmail.com>
2024-05-11 10:15:27 -0500

    Cambios front

--- consultarMusicos.php
+++ consultarMusicos.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mostrar Datos de Musicos</title>
+    <title>Musicos</title>
     <link rel="stylesheet" href="style.css"> <!-- Incluye tu archivo CSS externo -->
 </head>
 <body>
